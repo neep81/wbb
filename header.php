efdc5ce2baeb9a94063fb36dc107942f269943f7
KaushikIyer16 <kaushiiyer@gmail.com>
2017-12-30 03:15:07 +0530

    v1.5.2

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
   <nav>
     <div class="nav-wrapper wbb-navbar">
       <a href="index.php" class="brand-logo"><img src="./resources/wbb_high.png" alt="" class="img-responsive" width="120"></a>
-      <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="fa fa-bars" aria-hidden="true"></i></a>
+      <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="fa fa-bars wbb-hamburger " aria-hidden="true"></i></a>
       <ul id="nav-mobile" class="right hide-on-med-and-down">
         <li><a href="about.php">About Us</a></li>
         <li><a href="gallery.php">Gallery</a></li>
@@ -12,13 +12,26 @@
         <li><a href="index.php#wbb-membership">Contact Us</a></li>
       </ul>
 
-      <ul class="side-nav" id="mobile-demo">
+      <ul class="side-nav wbb-side-nav" id="mobile-demo">
+        <li><div class="user-view">
+
+              <a href="#!user"><img class="circle" src="./resources/wbb_high.png"></a>
+              <br>
+              <br>
+            </div>
+        </li>
         <li><a href="about.php">About Us</a></li>
         <li><a href="gallery.php">Gallery</a></li>
         <li><a href="schedule.php">ISL Schedule</a></li>
         <li><a href="chants.php">Chants</a></li>
         <li><a href="about.php#wbb-register">Register</a></li>
         <li><a href="index.php#wbb-membership">Contact Us</a></li>
+        <li>
+          <a href="https://www.facebook.com/WestBlockBlues/" target="_blank" style="display:inline-block;"><i class="fa fa-2x fa-facebook"></i></a>
+          <a href="https://twitter.com/WestBlockBlues" target="_blank" style="display:inline-block;"><i class="fa fa-2x fa-twitter"></i></a>
+          <a href="https://www.instagram.com/westblockblues/" target="_blank" style="display:inline-block;"><i class="fa fa-2x fa-instagram" aria-hidden="true"></i></a>
+          <a href="https://www.youtube.com/channel/UCiz53QWWrGH7df3nGjrTJZA" target="_blank" style="display:inline-block;"><i class="fa fa-2x fa-youtube-play"></i></a>
+        </li>
       </ul>
     </div>
 
